6e31173bd9b3447f3bdbdf1f81d48835360d765d
Mourya Prasanth <mouryaprasanthp@gmail.com>
2021-06-24 17:46:54 +0530

    add progressbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,21 @@
-import "../styles/globals.css";
+import React from 'react';
 import { ThemeProvider } from 'theme-ui';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import theme from '../src/utils/theme.js';
 import Layout from '../components/layout';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
+NProgress.configure({ showSpinner: false });
+Router.events.on('routeChangeStart', () => {
+  NProgress.start();
+});
+Router.events.on('routeChangeComplete', () => {
+  NProgress.done();
+});
+Router.events.on('routeChangeError', () => NProgress.done());
+
+function App({ Component, pageProps }) {
   // return <Component {...pageProps} />;
   return (
     <ThemeProvider theme={theme}>
@@ -14,4 +26,4 @@ function MyApp({ Component, pageProps }) {
   );
 }
 
-export default MyApp;
+export default App;
