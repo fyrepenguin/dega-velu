1803ab64f936b914d7456e4d2786fb5db306e458
Mourya Prasanth <mouryaprasanthp@gmail.com>
2022-03-03 15:22:32 +0530

    add env variables with different names

--- store/client.js
+++ store/client.js
@@ -4,7 +4,7 @@ import 'isomorphic-unfetch';
 export const client = new ApolloClient({
   uri: process.env.API_URL,
   headers: {
-    'X-Dega-API-Key': `${process.env.DEGA_API_KEY}`,
+    'X-Dega-API-Key': `${process.env.DEGA_API_KEY || process.env.DEGA_API_KEY_TWO || process.env.DEGA_API_KEY_THREE || process.env.DEGA_API_KEY_FOUR}`,
     'X-Space': parseInt(process.env.SPACE_ID),
   },
   cache: new InMemoryCache(),
