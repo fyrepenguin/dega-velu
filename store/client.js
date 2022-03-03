5454b458c41cc791170676ace62603836d91293b
Mourya Prasanth <mouryaprasanthp@gmail.com>
2022-03-03 15:47:18 +0530

    add checks for variables

--- store/client.js
+++ store/client.js
@@ -4,7 +4,7 @@ import 'isomorphic-unfetch';
 export const client = new ApolloClient({
   uri: process.env.API_URL,
   headers: {
-    'X-Dega-API-Key': `${process.env.DEGA_API_KEY || process.env.DEGA_API_KEY_TWO || process.env.DEGA_API_KEY_THREE || process.env.DEGA_API_KEY_FOUR}`,
+    'X-Dega-API-Key': `${(process.env.DEGA_API_KEY.length > 0 && process.env.DEGA_API_KEY) || (process.env.DEGA_API_KEY_TWO.length > 0 && process.env.DEGA_API_KEY_TWO) || (process.env.DEGA_API_KEY_THREE.length > 0 && process.env.DEGA_API_KEY_THREE) || (process.env.DEGA_API_KEY_FOUR.length > 0 && process.env.DEGA_API_KEY_FOUR)}`,
     'X-Space': parseInt(process.env.SPACE_ID),
   },
   cache: new InMemoryCache(),
